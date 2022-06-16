144285d8c843488841ac414fdb5106d7c56d5eba
sergiop45 <sergiopsantos33@gmail.com>
2022-06-16 09:10:48 -0300

    conversor1

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Conversor from './components/Conversor'
 function App() {
   return (
     <div className="App">
-      
+    <Conversor moedaA="BRL" moedaB="USD" />
      <Conversor moedaA="USD" moedaB="BRL" />
     </div>
   );
